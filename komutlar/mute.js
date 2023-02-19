07538518edee8d02da05a1498d973206195c8d8f
Glitch (cedar-emphasized-ceiling) <none>
2023-02-19 17:12:43 +0000

    🚕🕴 Checkpoint ./unmute.js:23699141/4283 ./komutlar/unmute.js:23699141/3537 ./komutlar/mute.js:23699141/3934

--- komutlar/mute.js
+++ komutlar/mute.js
@@ -1,55 +1,60 @@
-const Discord = require('discord.js')
+const Discord = require("discord.js");
+const ms = require("ms");
+const ayarlar = require('../ayarlar.json');
+const prefix = ayarlar.prefix;
 
 
+var mutelirolu = "cezalı" //MUTELENDİGİ ZAMAN VERİLECEK ROLU  BURAYA YAZINIZ...
 
-    exports.run = async(client, message, args) => {
-    
-		if(!message.member.roles.cache.has("1076070813834416160")){
-            const yetkiyok = new Discord.MessageEmbed()
-            .setDescription(`${message.author} **Bu kodu kullanmak için gerekli yetkiye sahip değilsin.**`)
-            .setColor('#ff0000')
-            return message.channel.send(yetkiyok)
-        }
-    
-        let kullanıcı = message.mentions.members.first();
-        let sebep = args.slice(1).join(' ')
+module.exports.run = async (bot, message, args) => {
 
-        if(!kullanıcı){
-            const cmfhata = new Discord.MessageEmbed()
-            .setColor('#ff0000')
-            .setDescription(`**Lütfen Kullanıcı Belirt.**`)
-            return message.channel.send(cmfhata)
-        }
+  let mutekisi = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!mutekisi) return message.reply(`:warning: Lütfen bir kullanıcı etiketleyiniz! \nDoğru Kullanım; \`${prefix}mute <@kullanıcı> <1sn/1dk/1sa/1g>\``)
+  if(mutekisi.hasPermission("MANAGE_MESSAGES")) return message.reply(`:warning: Yetkili bir kişiyi muteleyemem! \nDoğru Kullanım; \`${prefix}mute <@kullanıcı> <1sn/1dk/1sa/1g>\``)
+  let muterol = message.guild.roles.find(`name`, mutelirolu);
+  if(!muterol){
+    try{
+      muterol = await message.guild.createRole({
+        name: mutelirolu,
+        color: "#000000",
+        permissions:[]
+      })
+      message.guild.channels.forEach(async (channel, id) => {
+        await channel.overwritePermissions(muterol, {
+          SEND_MESSAGES: false,
+          ADD_REACTIONS: false
+        });
+      });
+    }catch(e){
+      console.log(e.stack);
+    }
+  }
+  let mutezaman = args[1]
+  .replace(`sn`, `s`)
+  .replace(`dk`, `m`)
+  .replace(`sa`, `h`)
+  .replace(`g`, `d`)
 
-        if(!sebep){
-            const cmfhata = new Discord.MessageEmbed()
-            .setColor('#ff0000')
-            .setDescription(`**Lütfen Sebep Belirt**`)
-            return message.channel.send(cmfhata)
-        }
-        
-        if(kullanıcı && sebep){
-            const cmfmute = new Discord.MessageEmbed()
-            .setColor('#00ff00')
-            .setDescription(`${kullanıcı} Kişisine ${message.author} Tarafından **${sebep}** Sebebi İle Mute Atıldı.`)
-            .setFooter(kullanıcı.user.username + " Umarız Hatalarını Birdaha Tekrarlamazsın...")
-            .setThumbnail(kullanıcı.user.avatarURL({dynamic: true, size: 2048}))
-            message.channel.send(cmfmute)
+  if(!mutezaman) return message.reply(`:warning: Lütfen bir zaman giriniz! \nDoğru Kullanım; \`${prefix}mute <@kullanıcı> <1sn/1dk/1sa/1g>\``)
 
-            // Mute Atıldığında Verilecek & Alınacak Roller
-            kullanıcı.roles.add('1076909184689381476')
-            kullanıcı.roles.remove('1076456272213332078')
-        }
+  await(mutekisi.addRole(muterol.id));
+  message.reply(`<@${mutekisi.id}> kullanıcısı ${args[1]} süresi boyunca mutelendi!`);
 
-    } // CodeMareFi - #MareFi && #CMF
+  setTimeout(function(){
+    mutekisi.removeRole(muterol.id);
+    message.channel.send(`<@${mutekisi.id}> kullanıcısının mutelenme süresi sona erdi!`);
+  }, ms(mutezaman));
+}
 
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: ['Mute','MUTE','sustur','Sustur','SUSTUR'],
+    aliases: [],
     permLevel: 0
-}
-
-exports.help = {
-    name: 'mute'
-}
+  };
+  
+  exports.help = {
+    name: "mute",
+    description: "Etiketlediğiniz kişiye belirttiğiniz süre kadar mute atar.",
+    usage: "mute @kullanıcı 1sn/1dk/1sa/1g"
+  };
